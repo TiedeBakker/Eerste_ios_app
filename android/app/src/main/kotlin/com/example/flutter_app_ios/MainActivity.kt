1f4c33cf741020b90adbaf62a486e088bd3a121b
Tiede Bakker <tiede.bakker@gmail.com>
2025-11-18 23:40:35 +0100

    First commit: initial Flutter project

---
+++ android/app/src/main/kotlin/com/example/flutter_app_ios/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_app_ios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo iOS App vanaf Windows',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter iOS demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Je hebt op de knop gedrukt:',
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              '$counter',
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
